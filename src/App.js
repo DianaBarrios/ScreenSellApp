072e1a663e318f6cb6ff0af385a78d7593d5cfde
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-12-14 01:17:02 -0800

    adding my files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      <h1 class="display-3">
+        <p>
+          Bienvenido a <span class="font-weight-bold text-danger">Screensell</span>
+        </p>
+        
+      </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
