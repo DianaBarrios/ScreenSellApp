3d9b9c70f830f8089188e7d23e35b872ee92a7b3
Diana Barrios <dianabarriosmacias@hotmail.com>
2020-01-22 00:36:35 -0800

    header buy contact map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,87 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LOGO from './LOGO-SS.png';
+import FBLOGO from './FB-LOGO.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      <h1 class="display-3">
-        <p>
-          Bienvenido a <span class="font-weight-bold text-danger">Screensell</span>
-        </p>
-        
-      </h1>
-      </header>
+
+      <header className="App-header"></header> 
+
+      <div class="container my-5">
+        <h1>
+          <p>
+          ¡La mayor <span class="font-weight-bold text-danger">variedad</span> de marcas disponibles!
+          </p>
+        </h1>
+      </div>
+
+      <div class="container my-5 px-4">
+        <div class="row justify-content-md-center">
+          <div class="card text-center">
+            
+            <div class="card-header">
+              CATÁLOGO EN LÍNEA
+            </div>
+
+            <div class="card-body">
+              <h5 class="card-title">¡Hacemos envíos a toda la república!</h5>
+              <p class="card-text">Conoce todas las pantallas táctiles para tablets y celulares que tenemos en <span class="font-weight-bold text-warning">Mercado Libre</span></p>
+              <p class="card-text font-weight-bold">Venta a mayoreo y menudeo</p>
+              <a href="https://listado.mercadolibre.com.mx/touch_CustId_444437955#D[A:touch,SI:444437955]" class="btn btn-warning btn-lg">Comprar</a>
+            </div>
+
+            <div class="card-footer text-muted">
+              SCREEN-SELL 
+            </div>
+          </div>
+        </div>
+      </div> 
+
+          <div class="container-fluid my-5 App-contact">
+            <div class="row justify-content-md-center px-5">
+          
+            <div class="card my-4" style={{width: 450}}>
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <a href="https://www.facebook.com/screensell">
+                  <img src={LOGO} className="card-img" alt="screen-sell" style={{width: 200, padding: 5}}/>
+                </a>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">PONTE EN CONTACTO</h5>
+                  <p class="card-text">Si tienes alguna duda, escríbenos a <a href="mailto:screensellsinaloa@gmail.com?Subject=Dudas" target="_top">screensellsinaloa@gmail.com</a> o manda un mensaje a nuestras páginas de Facebook.</p>
+                  
+                  <p class="card-text">Tijuana 
+                    <a href="https://www.facebook.com/screensell">
+                      <span class="text-decoration-none"> @screensell</span>
+                    </a> <p><small>+664-638-4869</small></p>
+                  </p>
+
+                  <p class="card-text">Culiacán
+                    <a href="https://www.facebook.com/screensellculiacan/">
+                    <span class="text-decoration-none"> @screensellculiacan</span>
+                    </a> <p><small>+667-258-5082</small></p>
+                  </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+
+          <div class="container my-5">
+            <div class="row mx-5 mb-3 justify-content-center">
+              <h2>Nuestras ubicaciones</h2>
+            </div>
+            <div class="row mx-3 justify-content-center">
+              <iframe src="https://www.google.com/maps/d/embed?mid=1QK_YS8xR0CuxuHy8GtRZLrvbpOr-N024" width="500" height="400"></iframe>
+            </div>
+          </div>
+
     </div>
   );
 }
