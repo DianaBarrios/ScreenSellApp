9d4d984fd2c156282e2916d3319b08876c6939ad
Diana Barrios <dianabarriosmacias@hotmail.com>
2020-01-22 21:06:55 -0800

    build bundle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LOGO from './LOGO-SS.png';
-import FBLOGO from './FB-LOGO.png';
 
 function App() {
   return (
